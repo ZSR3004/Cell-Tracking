ac7098742cf05b820995e2a55ce01e29250ece4f
Jamie Sloves <jsloves@wesleyan.edu>
2025-10-10 16:34:00 -0400

    Added more functions (get name, isolate channel, save video) to the skeleton code for the Tiff class

--- src/tiffclass.py
+++ src/tiffclass.py
@@ -3,14 +3,23 @@ class Tiff:
     This is a class that imports TIFF file to program, converts TIFF to numpy array using TIFFFILE,
     and stores the video type
     """
-    def __init__ (self, path, vid_type):
+    def __init__ (self, args):
         """
-        Initializes the class, saving the video path to the class as well as the video type
+        Initializes a TiffStack object by loading a TIFF file and extracting its frames.
+        Args:
+            path (str): Path to the TIFF file.
+            n_channels (int): Number of channels in the TIFF stack. Default is 3.
+            dtype (np.dtype): Data type of the image frames. Default is np.uint16.
+        
+        Attributes:
+            path (str): Path to the TIFF file.
+            timestamp (str): Timestamp of when the TIFF file was loaded.
+            tags (list): List of tags for each frame in the TIFF stack.
+            arr (np.ndarray): 4D numpy array containing the image frames, shape is (n_frames, n_channels, height, width
         """
-        self.path = path
-        self.vidtype = vid_type
+        raise NotImplementedError
     
-    def import_vid (self, path):
+    def import_vid (self, args):
         """
         Imports the TIFF file to the program
 
@@ -25,18 +34,53 @@ class Tiff:
         """
         raise NotImplementedError
     
-    def convert_to_numpy (self):
+    def _get_name(self) -> str:
+        """
+        Generates a name for the TiffStack based on the file name.
+        
+        Returns:
+            str: Name of the TiffStack.
+        """
+        raise NotImplementedError
+    
+    def isolate_channel(self, args):
+        """
+        Isolates a specific channel from the TIFF stack.
+
+        Args:
+            channel_idx (int): Index of the channel to isolate (0-indexed).
+
+        Returns:
+            np.ndarray: Isolated channel as a 3D numpy array.
+        """
+        raise NotImplementedError
+    
+    def convert_to_numpy (self, args):
         """
         Converts the TIFF file to a numpy array using TIFFFILE
 
         Args:
-        None
+        The channel you want to convert to a numpy
 
         Returns:
         numpy array
 
         Preconditions:
         Will have to call import_vid in this function
+        """
+
+        raise NotImplementedError
+
+    def save_orginal_video(self, args):
+        """
+        Saves a video of the original image frames from the TIFF stack.
+
+        Args:
+            idx (int): Index of the channel to visualize. Default is 0.
+            figsize (tuple): Figure size in inches (width, height). Default is (12, 8).
+
+        Returns:
+            None
         """
         raise NotImplementedError
     
