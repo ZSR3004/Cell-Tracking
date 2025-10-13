73f499187a5ff2b8e77f2a5ffe295fd19ea94485
Caroline Tracy <carolinerose207@gmail.com>
2025-10-12 22:28:34 -0400

    Did all of Caroline's skeleton code

---
+++ src/display_video.py
@@ -0,0 +1,5 @@
+"""
+Empty file for now.
+Part of the user interface that allows the user to watch the video inside our software.
+Note that we've already generated the video by now
+"""
---
+++ src/raft.py
@@ -0,0 +1,7 @@
+"""
+Empty for now. This file implements ML optical flow: RAFT. This is used for non-dyed and cytoplasm videos. 
+The set of functions in this file should iterate through frame pairs and generate a Numpy array of x, y coordinates representing vectors. 
+There should be multiple models trained with different sets of data.
+
+Potentially add in information done by Ziyad.
+"""
