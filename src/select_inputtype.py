4d7eb82709de1d3031df3f80e29b24d7f01d78de
Shital-Olee <shitaloli108@gmail.com>
2025-10-13 19:25:19 -0400

    implemented skeleton code for user input type

---
+++ src/select_inputtype.py
@@ -0,0 +1,30 @@
+"""
+Have the user select which type of video they are inputting into the software Types of videos:
+Nucleus is dyed
+Cytoplasm is dyed
+Nothing is dyed
+
+"""
+
+def get_video_type(args):
+
+    """
+    prints to the user the options for the type of video for input: 
+    1. Nucleus is dyed
+    2. Cytoplasm is dyed
+    3. Nothing is dyed
+
+    [need to direct based on the option
+    confirm the type that the user has chosen]
+    """
+    raise NotImplementedError
+
+
+def direct_video_type(args):
+
+    """
+    after the video type is received, perform actions/direct towards the 
+    action for the video
+    """
+
+    raise NotImplementedError
