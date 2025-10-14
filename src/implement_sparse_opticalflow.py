7fa09fd84263a47af0acf0a63302ce1c9cd3a9f8
Caroline Tracy <carolinerose207@gmail.com>
2025-10-14 16:37:46 -0400

    Fixed more typos

--- src/implement_sparse_opticalflow.py
+++ src/implement_sparse_opticalflow.py
@@ -3,7 +3,7 @@ import numpy as np
 def implement_lucas_kande(args) -> np.ndarray:
     """
     Use Lucas-Kanade to generate numpy arrays of sparse optical.
-    Take numpy array of orignal video
+    Take numpy array of original video
     Take pre-selected points
     Track said points, actual implementation of Lucas-Kanade across frames
     Output numpy array of (x, y) pairs for displacement
@@ -17,6 +17,6 @@ def save_optflow_video(flow, idx : int = 0, step : int = 20,
                           figsize : int | int = (12,8),
                           title : str = None, overlay : bool = False):
      """
-        Saves a video visualizing the optical flow using the lucas_kande.
+        Saves a video visualizing the optical flow using the Lucas_Kanade.
      """
      raise NotImplementedError
