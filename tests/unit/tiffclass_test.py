ffd2fc1a168e9576ef18e25781ca55590fc6d335
Jamie Sloves <jsloves@wesleyan.edu>
2025-10-14 21:09:25 -0400

    Created concepts of tests for initializing the tiff class and importing the video

--- tests/unit/tiffclass_test.py
+++ tests/unit/tiffclass_test.py
@@ -1,8 +1,26 @@
 import tiffclass as tiff
+from tiffclass import np
 import pytest
+from tiffclass import tf
 
-def init_test():
+def test_init(tmp_path):
     """
     Tests whether the Tiff class initializes correctly.
     """
+    fake_tiff_path = tmp_path / "test.tiff"
+    fake_tiff_path.write_bytes(b"Fake TIFF data")
+
+    x = tiff.Tiff(path=str(fake_tiff_path), n_channels=3, dtype=np.uint16)
+
+    assert x.path == str(fake_tiff_path)
+    assert x.n_channels == 3
+    assert x.dtype == np.uint16
+    assert hasattr(x, "timestamp")
+    assert hasattr(x, "arr")
+    assert hasattr(x, "tags")
+
+def test_import_vid(path):
+    assert isinstance (tiff.import_vid(path) == tf.TiffFile)
+    
+
 
