0ae46d462f87f73d2c252831d969895ad9a3a6a3
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-06 12:00:32 -0500

    Added helper tests for pad_to_multiple_of_8 test

--- tests/unit/raft_test.py
+++ tests/unit/raft_test.py
@@ -4,25 +4,60 @@ import numpy as np
 from src import raft
 from src import tiffclass as tiff
 
+
 @pytest.fixture
 def init_tiff():
-    original_path = "../../datasets/nuclei_labeled/20220929_MCF_Rab5a_WH_heterotypic_s1_SCALED.tif"
+    original_path = (
+        "../../datasets/nuclei_labeled/20220929_MCF_Rab5a_WH_heterotypic_s1_SCALED.tif"
+    )
     return tiff.Tiff(original_path)
+
+
+### pad_to_multiple_of_8 ###
+def _check_divisible_by_8():
+    raise NotImplementedError
+
+
+def _check_preserved_values():
+    raise NotImplementedError
+
+
+def _check_padding_behavior():
+    raise NotImplementedError
+
+
+def _check_no_change_over_device():
+    raise NotImplementedError
+
+
+def _check_idempotence():
+    raise NotImplementedError
+
 
 def test_pad_to_multiple_of_8():
     raise NotImplementedError
 
+
+### preprocess_tensor ###
 def test_preprocess_tensor():
     raise NotImplementedError
 
+
+### batch_frames ###
 def test_batch_frames():
     raise NotImplementedError
 
+
+### get_raft_optical_flow ###
 def test_get_raft_optical_flow():
     raise NotImplementedError
 
+
+### make_raft_output_array ###
 def test_make_raft_output_array():
     raise NotImplementedError
 
+
+### calcOpticalFlowRAFT ###
 def test_calcOpticalFlowRAFT():
     raise NotImplementedError
