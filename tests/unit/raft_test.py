6d441bfd86dd2b2d2da39ba27a9abe0d985faa11
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-06 11:48:10 -0500

    restructured tests for modified raft.py file structure

--- tests/unit/raft_test.py
+++ tests/unit/raft_test.py
@@ -9,15 +9,20 @@ def init_tiff():
     original_path = "../../datasets/nuclei_labeled/20220929_MCF_Rab5a_WH_heterotypic_s1_SCALED.tif"
     return tiff.Tiff(original_path)
 
-def test_make_tiff_into_tensor():
+def test_pad_to_multiple_of_8():
+    raise NotImplementedError
+
+def test_preprocess_tensor():
     raise NotImplementedError
 
 def test_batch_frames():
     raise NotImplementedError
 
-def test_calculate_raft_optical_flow():
+def test_get_raft_optical_flow():
     raise NotImplementedError
 
 def test_make_raft_output_array():
     raise NotImplementedError
 
+def test_calcOpticalFlowRAFT():
+    raise NotImplementedError
