c2874db3b6cf616605032147f6b873f0005e2b32
Caroline Tracy <carolinerose207@gmail.com>
2025-11-20 20:37:26 -0500

    Continued working on test_save_optical_flow_as_matlab in saving_test.py

--- tests/unit/saving_test.py
+++ tests/unit/saving_test.py
@@ -264,12 +264,15 @@ def test_save_optical_flow_as_matlab(init_tiff: tuple, tmp_path):
     save_dir = tmp_path / name1
     assert not save_dir.exists()
 
+    print(tiff_arr.shape)
+
+    """
     optical_flow_channel0 = flow.optical_flow(arr=tiff_arr, channel=0)
     optical_flow_channel1 = flow.optical_flow(arr=tiff_arr, channel=1)
     optical_flow_channel2 = flow.optical_flow(arr=tiff_arr, channel=2)
     optical_flow_channel0_custom = flow.optical_flow(arr=tiff_arr, channel=0, pyr_scale=0.25, levels=2, winsize=30, iterations=5, poly_n=7, poly_sigma=2.8, flags=1)
-    optical_flow_channel1_custom = flow.optical_flow(arr=tiff_arr, channel=1, pyr_scale=1.0, levels=5, winsize=10, iterations=9, poly_n=11, poly_sigma=3.2, flags=3)    
-    optical_flow_channel2_custom = flow.optical_flow(arr=tiff_arr, channel=2, pyr_scale=0.75, levels=4, winsize=5, iterations=2, poly_n=3, poly_sigma=0.2, flags=2)    
+    optical_flow_channel1_custom = flow.optical_flow(arr=tiff_arr, channel=1, pyr_scale=0.45, levels=5, winsize=10, iterations=9, poly_n=11, poly_sigma=3.2, flags=3)    
+    optical_flow_channel2_custom = flow.optical_flow(arr=tiff_arr, channel=2, pyr_scale=0.3, levels=4, winsize=5, iterations=8, poly_n=3, poly_sigma=0.2, flags=2)    
     calculate_optical_flow = flow.calculate_optical_flow(arr=tiff_arr)
     calculate_optical_flow_default_true = flow.calculate_optical_flow(arr=tiff_arr, default=True)
 
@@ -280,6 +283,7 @@ def test_save_optical_flow_as_matlab(init_tiff: tuple, tmp_path):
     optical_flow_channel0_arr = optical_flow_channel0_data['optical_flow']
     assert isinstance(optical_flow_channel0_arr, np.ndarray)
     assert np.array_equal(optical_flow_channel0_arr, optical_flow_channel0)
+    """
 
     #DO THIS FOR EVERY OTHER ONE (optical_flow_channel1, optical_flow_channel2, ETC!)
 
