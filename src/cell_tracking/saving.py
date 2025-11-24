cbb8593bb0345c901b76125d47668b26564c8a15
Caroline Tracy <carolinerose207@gmail.com>
2025-11-23 20:12:31 -0500

    Rewrote save_optical_flow_as_xyz in saving.py. Rewrote it using the code Ziyad wrote in the summer.

--- src/cell_tracking/saving.py
+++ src/cell_tracking/saving.py
@@ -4,8 +4,9 @@ import numpy as np
 import pathlib
 from pathlib import Path
 import matplotlib.animation as animation
-import xyz_py
 import scipy.io
+import gc
+import fileprocessingutils as fputil
 from scipy.io import savemat
 from . import tiffclass as tiff
 from .defaults import default_process, default_flow
@@ -53,29 +54,24 @@ def save_arr(name: str, tiff_instance: tiff.Tiff, main_path: str) -> None:
     np.save(save_path, tiff_arr)
 
 
-def save_optical_flow_as_xyz(name: str, opt_flow: np.ndarray, main_path: str) -> None:
+def save_optical_flow_as_xyz(opt_flow : np.ndarray, save_path : str) -> None:
     """
-    Saves the optical flow array as an XYZ file.
+    Converts the optical flow numpy arrays to XYZ format and saves them.
+    The XYZ files are saved in the 'flow_xyz' directory.
 
     Args:
-        name (str): Name of the numpy array.
-        opt_flow (np.ndarray): The optical flow array.
-        main_path (str): Main path to the directory.
+        opt_flow (np.ndarray): The optical flow numpy array.
+        save_path (str): Path to save the XYZ file to.
 
     Returns:
-        None: Just saves the optical flow array to a file.
+        None.
     """
-    save_path = get_unique_path(name, lambda i: f"{name}_flow{i}.xyz", main_path)
+    opt_flow_c0 = opt_flow[:, 0, ...]
+    xyz_save_path = save_path.replace('.npy', '.pt').replace('flow_arr', 'flow_xyz')
+    fputil.saveas_xyz(opt_flow_c0, xyz_save_path)
 
-    dx_dy_arr = opt_flow.reshape(-1, 2)
-    zeros = np.zeros((len(dx_dy_arr), 1), dtype=dx_dy_arr[0][0].dtype)
-
-    xyz_arr = np.hstack((dx_dy_arr, zeros))
-    labels_arr = ["I"] * len(dx_dy_arr)
-
-    xyz_py.save_xyz(
-        f_name=str(save_path), labels=labels_arr, coords=xyz_arr, comment=None
-    )
+    del opt_flow_c0
+    gc.collect()
 
 
 def save_optical_flow_as_matlab(
