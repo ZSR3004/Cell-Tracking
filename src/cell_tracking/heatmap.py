ff9f08975d4b909932985bfa28b14c21ee9509f9
Caroline Tracy <carolinerose207@gmail.com>
2025-11-28 19:40:39 -0500

    Created heatmap_test.py file

--- src/cell_tracking/heatmap.py
+++ src/cell_tracking/heatmap.py
@@ -12,7 +12,7 @@ def vector_magnitude_heatmaps(flow, normalize=True):
     Args:
         flow (np.ndarray): Array of shape (frames, height, width, 2) with (dx, dy) vectors.
         apply_colormap (bool): If True, returns heatmaps with color (BGR, 3-channel).
-        normalize (bool): If True, normalizes magnitudes to 0–255 range for visualization.
+        normalize (bool): If True, normalizes magnitudes to 0-255 range for visualization.
 
     Returns:
         heatmaps (np.ndarray): Array of shape (frames, height, width) or (frames, height, width, 3)
---
+++ tests/unit/heatmap_test.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+# Add the project root (Cell-Tracking) to sys.path
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+import cv2, pytest
+from src.cell_tracking import heatmap
+from src.cell_tracking import tiffclass as tiff
+from matplotlib import animation
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.colors as colors
+from unittest.mock import patch, Mock, MagicMock
+
+TIFF_PATHS = [
+    (
+        "datasets/nuclei_labeled/20220929_MCF_Rab5a_WH_heterotypic_s1_SCALED.tif",
+        (96, 3, 520, 2329),
+    )
+]
+
+
+@pytest.fixture(params=TIFF_PATHS)
+def init_tiff(request: pytest.FixtureRequest) -> tiff.Tiff:
+    """
+    Creates a Tiff class instance.
+
+    Args:
+        request (pytest.FixtureRequest): The paths to generate Tiff instances from.
+
+    Returns:
+        (tiff.Tiff): Tiff class instance of the path.
+    """
+    path, info = request.param
+    return (tiff.Tiff(path), info)
+
+
+def
