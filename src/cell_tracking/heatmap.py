2b6c85f0e1ebc0685a9f300b85c7c607b1912834
Ziyad Rahman <zrahman3004@gmail.com>
2025-12-02 17:08:11 -0500

    Updated docstring for endpoint

--- src/cell_tracking/heatmap.py
+++ src/cell_tracking/heatmap.py
@@ -11,8 +11,8 @@ def plot_heatmap(arr: np.ndarray, title: str, output_path: str, fps: int = 30) -
 
     Args:
         arr (np.ndarray): The array to create a heatmap out of. This
-            should be shape (f, h, w, 2) where 2 is (dx, dy). In other
-            words, it is a single channel in cartesian coordinates.
+            should be shape (f, c, h, w, 2) where 2 is (dx, dy) 
+            which is in cartesian coordinates.
         title (str): Title for the heatmap.
         output_path (str): The path for the file to be saved to.
         fps (int): Fps of the output heatmap video.
