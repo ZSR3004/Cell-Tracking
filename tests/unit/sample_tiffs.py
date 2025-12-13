0eddcd7a6644f4191815fe2d470be27c9e0018df
Ziyad Rahman <zrahman3004@gmail.com>
2025-12-12 20:55:06 -0500

    Added sample tiff list

---
+++ tests/unit/sample_tiffs.py
@@ -0,0 +1,15 @@
+TIFF_PATHS = [
+    "datasets/audrey_clay-streib_MCFRab5a.tif",
+    "datasets/liam_jones_1.tiff",
+    "datasets/liam_jones_2.tiff",
+    "datasets/liam_jones_3.tiff",
+    "datasets/liam_jones_4.tiff",
+    "datasets/liam_jones_5.tiff",
+    "datasets/liam_jones_6.tiff",
+    "datasets/liam_jones_7.tiff",
+    "datasets/liam_jones_8.tiff",
+    "datasets/liam_jones_9.tiff",
+    "datasets/liam_jones_10.tiff",
+    "datasets/liam_jones_11.tiff",
+    "datasets/liam_jones_12.tiff"
+]
