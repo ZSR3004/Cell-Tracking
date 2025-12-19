f494e983302619bd3a5a044b274478cfbba207cc
Caroline Tracy <carolinerose207@gmail.com>
2025-12-18 23:28:57 -0500

    Started test_create_vector_field_video in vector_magnitude_map_test.py

---
+++ tests/unit/vector_magnitude_map_test.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+# Add the project root (Cell-Tracking) to sys.path
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+import pytest, tifffile
+from unittest.mock import patch, Mock, MagicMock, ANY
+from multiprocessing import Pool, cpu_count
+from src.cell_tracking import tiffclass as tiff
+import numpy as np
+
+TIFF_PATHS = ["datasets/20220929_MCF_Rab5a_WH_heterotypic_s1_SCALED.tif"]
+
+
+@pytest.fixture(params=TIFF_PATHS)
+def init_tiff(request: pytest.FixtureRequest):
+    """
+    Creates a Tiff class instance.
+
+    Args:
+        request (pytest.FixtureRequest): The paths to generate Tiff instances from.
+
+    Returns:
+        (tiff.Tiff): A tuple containing information about the TIFF file.
+            - img (str): A TIFF instance.
+            - f (int): Number of frames.
+            - c (int): Number of channels.
+            - h (int): Height.
+            - w (int): Width.
+    """
+    path = request.param
+    img = tifffile.imread(path)
+    info = (img.shape[0], img.shape[1], img.shape[2], img.shape[3])
+    return (tiff.Tiff(path), info)
+
+
+def test_create_vector_field_video()
+"""
+Cont once I get the answers to these questions:
+Where do you get arr and og_arr from? In other words, what function do you pass the original array through to get arr and og_arr? 
+Also shouldn't arr's shape be (T-1, H, W, 2)?
+"""
