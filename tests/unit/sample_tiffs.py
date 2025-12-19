6056f629fb2871f68d84139031e172432336585a
Ziyad Rahman <zrahman3004@gmail.com>
2025-12-19 12:55:27 -0500

    Created module TIFF_PATHS

--- tests/unit/sample_tiffs.py
+++ tests/unit/sample_tiffs.py
@@ -1,15 +1,5 @@
-TIFF_PATHS = [
-    "datasets/audrey_clay-streib_MCFRab5a.tif",
-    "datasets/liam_jones_1.tiff",
-    "datasets/liam_jones_2.tiff",
-    "datasets/liam_jones_3.tiff",
-    "datasets/liam_jones_4.tiff",
-    "datasets/liam_jones_5.tiff",
-    "datasets/liam_jones_6.tiff",
-    "datasets/liam_jones_7.tiff",
-    "datasets/liam_jones_8.tiff",
-    "datasets/liam_jones_9.tiff",
-    "datasets/liam_jones_10.tiff",
-    "datasets/liam_jones_11.tiff",
-    "datasets/liam_jones_12.tiff"
-]
+import os
+
+TIFF_PATHS = [os.path.join("datasets/", f) for f in os.listdir("datasets/") if (not f.endswith('.md'))]
+
+print(TIFF_PATHS)
