2eed59c19ce662ee6713177f45c564e1fc9daba0
Ziyad Rahman <zrahman3004@gmail.com>
2025-12-19 10:18:42 -0500

    Deprecated tests for a deprecated feature

--- tests/unit/raft_test.py
+++ tests/unit/raft_test.py
@@ -13,6 +13,8 @@ from unittest.mock import Mock, patch
 
 TIFF_PATHS = ["datasets/20220929_MCF_Rab5a_WH_heterotypic_s1_SCALED.tif"]
 
+pytestmark = pytest.mark.skip(reason="We were in the middle of updating this module when we decided to deprecate RAFT. The tests are not expected to pass, but we worked hard on them, so we are not deleting them. Please run pytest - m 'no deprecated' to avoidthis module which may take considerable time to run.")
+
 
 @pytest.fixture(params=TIFF_PATHS)
 def init_tiff(request: pytest.FixtureRequest):
